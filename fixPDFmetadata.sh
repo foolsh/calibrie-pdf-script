3086a2d089b450735a81ea69928cf8ec9ddea877
Benjamin Brown <foolsh.person@gmail.com>
2018-12-05 17:36:25 -0500

    Not perfect but works

--- fixPDFmetadata.sh
+++ fixPDFmetadata.sh
@@ -1,2 +1,20 @@
 #!/bin/sh
 
+# Requirements: libimage-exiftool-perl
+
+# Change to the calibrie library root and execute this script
+# Only effects PDF files so its safe to run in a mixed Library
+
+find $1 -mindepth 1 -maxdepth 1 -type d | while read -r dir
+    do
+        [ -d $dir ]
+        cd "$dir"
+        echo Entering into $dir and executing exiftool
+
+        for i in *.pdf ; do
+            exiftool -overwrite_original -Title="$i" "$i"
+            #sleep 1
+        done
+        cd ../
+    done;
+
